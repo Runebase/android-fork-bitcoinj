bf37d48c79d16c3c1ac486e542269756f2be5df9
bobpepers <40283955+bobpepers@users.noreply.github.com>
2018-09-06 22:45:57 +0200

    Update RunebaseMainNetParams.java

--- core/src/main/java/org/bitcoinj/params/RunebaseMainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/RunebaseMainNetParams.java
@@ -18,38 +18,37 @@ public class RunebaseMainNetParams extends AbstractBitcoinNetParams {
         interval = INTERVAL;
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
-        dumpedPrivateKeyHeader = 128;
-        addressHeader = 58;
-        p2shHeader = 50;
+        dumpedPrivateKeyHeader = 216;
+        addressHeader = 60;
+        p2shHeader = 123;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 8333;
-        packetMagic = 0xf9beb4d9L;
-        bip32HeaderPub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
-        bip32HeaderPriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
+        packetMagic = 0xcff1d3a6L; 
+        bip32HeaderPub = 0x0586c22e; //The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderPriv = 0x0586dcf1; //The 4 byte header that serializes in base58 to "xprv"
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
         majorityRejectBlockOutdated = MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED;
         majorityWindow = MAINNET_MAJORITY_WINDOW;
 
         genesisBlock.setDifficultyTarget(0x1d00ffffL);
-        genesisBlock.setTime(1231006505L);
-        genesisBlock.setNonce(2083236893);
+        genesisBlock.setTime(1530246365L);
+        genesisBlock.setNonce(192857);
         id = ID_RUNEBASE_MAINNET;
-        subsidyDecreaseBlockCount = 210000;
-        spendableCoinbaseDepth = 100;
+        subsidyDecreaseBlockCount = 525960000;
+        spendableCoinbaseDepth = 100; //?
         String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
+        checkState(genesisHash.equals("0000c5edbe6f9c21b0c77e568b8339dc5e8ffdf78fe14fd3b1a2ea51c7f710fd"),
                 genesisHash);
 
         // This contains (at a minimum) the blocks which are not BIP30 compliant. BIP30 changed how duplicate
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(91722, Sha256Hash.wrap("00000000000271a2dc26e7667f8419f2e15416dc6955e5a6c6cdf3f2574dd08e"));
-        checkpoints.put(91812, Sha256Hash.wrap("00000000000af0aed4792b1acee3d966af36cf5def14935db8de83d6f9306f2f"));
-        checkpoints.put(91842, Sha256Hash.wrap("00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec"));
-        checkpoints.put(91880, Sha256Hash.wrap("00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721"));
-        checkpoints.put(200000, Sha256Hash.wrap("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf"));
+        checkpoints.put(0, Sha256Hash.wrap("0000c5edbe6f9c21b0c77e568b8339dc5e8ffdf78fe14fd3b1a2ea51c7f710fd"));
+        checkpoints.put(2000, Sha256Hash.wrap("000078083638b47e82675d657be9a31f7d36119186fb14bfee72fe3e6d78d488"));
+        checkpoints.put(2583, Sha256Hash.wrap("371e278ce135b6a63a0b10ba73c0a54b4745e5b95118362a75d77313095f1ce6"));
+        checkpoints.put(21000, Sha256Hash.wrap("ea8336433fbd537144c2f7a7079829d624c3515f91220de186c02ff9dddd2366"));
 
         dnsSeeds = new String[] {
                 "seed.bitcoin.sipa.be",         // Pieter Wuille
