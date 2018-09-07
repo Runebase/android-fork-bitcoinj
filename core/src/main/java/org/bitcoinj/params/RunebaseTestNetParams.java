0aef035f9311734a59952e4100e4130b9b802186
bobpepers <40283955+bobpepers@users.noreply.github.com>
2018-09-07 13:21:32 +0200

    Update RunebaseTestNetParams.java

--- core/src/main/java/org/bitcoinj/params/RunebaseTestNetParams.java
+++ core/src/main/java/org/bitcoinj/params/RunebaseTestNetParams.java
@@ -9,44 +9,48 @@ import java.net.*;
 import static com.google.common.base.Preconditions.*;
 
 
-public class RunebaseTestNetParams extends AbstractBitcoinNetParams {
+public class QtumTestNetParams extends AbstractBitcoinNetParams {
     public static final int MAINNET_MAJORITY_WINDOW = 1000;
     public static final int MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED = 950;
     public static final int MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE = 750;
 
-    public RunebaseTestNetParams() {
+    public QtumTestNetParams() {
         super();
         interval = INTERVAL;
         targetTimespan = TARGET_TIMESPAN;
         maxTarget = Utils.decodeCompactBits(0x1d00ffffL);
-        dumpedPrivateKeyHeader = 229;
-        addressHeader = 11;
-        p2shHeader = 106;
+        dumpedPrivateKeyHeader = 239;
+        addressHeader = 120;
+        p2shHeader = 110;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader };
         port = 8333;
-        packetMagic = 0xacb2d52dL;
-        bip32HeaderPub = 0x053782bf; //The 4 byte header that serializes in base58 to "xpub".
-        bip32HeaderPriv = 0x053784a4; //The 4 byte header that serializes in base58 to "xprv"
+        packetMagic = 0xf9beb4d9L;
+        bip32HeaderPub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderPriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
 
         majorityEnforceBlockUpgrade = MAINNET_MAJORITY_ENFORCE_BLOCK_UPGRADE;
         majorityRejectBlockOutdated = MAINNET_MAJORITY_REJECT_BLOCK_OUTDATED;
         majorityWindow = MAINNET_MAJORITY_WINDOW;
 
         genesisBlock.setDifficultyTarget(0x1d00ffffL);
-        genesisBlock.setTime(1529488943L);
-        genesisBlock.setNonce(13021);
-        id = ID_RUNEBASE_TESTNET;
-        subsidyDecreaseBlockCount = 525960000;
+        genesisBlock.setTime(1231006505L);
+        genesisBlock.setNonce(2083236893);
+        id = ID_QTUM_TESTNET;
+        subsidyDecreaseBlockCount = 210000;
         spendableCoinbaseDepth = 100;
         String genesisHash = genesisBlock.getHashAsString();
-        checkState(genesisHash.equals("00001c1c4c03ce2958600754ab7b20b5d0aef52c9472a7942a49175c0aa6268b"),
+        checkState(genesisHash.equals("000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"),
                 genesisHash);
 
         // This contains (at a minimum) the blocks which are not BIP30 compliant. BIP30 changed how duplicate
         // transactions are handled. Duplicated transactions could occur in the case where a coinbase had the same
         // extraNonce and the same outputs but appeared at different heights, and greatly complicated re-org handling.
         // Having these here simplifies block connection logic considerably.
-        checkpoints.put(0, Sha256Hash.wrap("00001c1c4c03ce2958600754ab7b20b5d0aef52c9472a7942a49175c0aa6268b"));
+        checkpoints.put(91722, Sha256Hash.wrap("00000000000271a2dc26e7667f8419f2e15416dc6955e5a6c6cdf3f2574dd08e"));
+        checkpoints.put(91812, Sha256Hash.wrap("00000000000af0aed4792b1acee3d966af36cf5def14935db8de83d6f9306f2f"));
+        checkpoints.put(91842, Sha256Hash.wrap("00000000000a4d0a398161ffc163c503763b1f4360639393e0e4c8e300e0caec"));
+        checkpoints.put(91880, Sha256Hash.wrap("00000000000743f190a18c5577a3c2d2a1f610ae9601ac046a38084ccb7cd721"));
+        checkpoints.put(200000, Sha256Hash.wrap("000000000000034a7dedef4a161fa058a2d67a173a90155f3a2fe6fc132e0ebf"));
 
         dnsSeeds = new String[] {
                 "seed.bitcoin.sipa.be",         // Pieter Wuille
@@ -109,10 +113,10 @@ public class RunebaseTestNetParams extends AbstractBitcoinNetParams {
         };
     }
 
-    private static RunebaseTestNetParams instance;
-    public static synchronized RunebaseTestNetParams get() {
+    private static QtumTestNetParams instance;
+    public static synchronized QtumTestNetParams get() {
         if (instance == null) {
-            instance = new RunebaseTestNetParams();
+            instance = new QtumTestNetParams();
         }
         return instance;
     }
